f51360f4bc9ab5b483b53bf7c55dc45e505931a1
Jason Verbeek <jvb@bigbrother.nl>
2023-11-09 08:42:26 +0100

    Initial commit with hacky rust-tools

---
+++ init.lua
@@ -0,0 +1,17 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
+
+rt.inlay_hints.enable()
