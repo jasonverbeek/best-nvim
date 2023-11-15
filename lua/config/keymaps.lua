05953871ccc8aa263249fd24aa99067a6dd725ac
Jason Verbeek <jason@localhost8080.org>
2023-11-15 08:05:23 +0100

    replace conjure with fireplace and add some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local util = require("lazyvim.util")
+
+local map = util.safe_keymap_set
+
+map("n", "<S-Left>", "<C-w>h", { desc = "Go to left split", remap = true })
+map("n", "<S-Down>", "<C-w>j", { desc = "Go to down split", remap = true })
+map("n", "<S-Up>", "<C-w>k", { desc = "Go to up split", remap = true })
+map("n", "<S-Right>", "<C-w>l", { desc = "Go to right split", remap = true })
