aeaa0d02cc32e1161ef503a619d52abcb230d0c2
Jason Verbeek <jvb@bigbrother.nl>
2024-08-02 21:23:45 +0200

    transparent tokyonight

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,5 +2,6 @@ return {
   { "folke/flash.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
+  { "ojroques/nvim-osc52", enabled = false },
   -- { "echasnovski/mini.indentscope", enabled = false },
 }
--- lua/plugins/tokyotonight.lua
+++ lua/plugins/tokyotonight.lua
@@ -2,7 +2,13 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
+  style = "storm",
   opts = {
     colorscheme = "tokyonight",
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
   },
 }
