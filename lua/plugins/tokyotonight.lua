b43df69424584f7f4e69e8f5527789e1c094b7a2
Jason Verbeek <jvb@bigbrother.nl>
2024-08-20 12:07:00 +0200

    some elements not transaprent

--- lua/plugins/tokyotonight.lua
+++ lua/plugins/tokyotonight.lua
@@ -2,13 +2,16 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  style = "storm",
+  -- style = "storm",
   opts = {
     colorscheme = "tokyonight",
     transparent = true,
+    --[[
     styles = {
       sidebars = "transparent",
       floats = "transparent",
     },
+    ]]
+    --
   },
 }
